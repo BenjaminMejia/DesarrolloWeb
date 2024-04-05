9d72fa74ca1235027cb5ea70ea9425ce58b04889
BenjaminMF <benmejia120@gmail.com>
2024-04-05 16:06:24 -0600

    comentarios

--- PrimerDepartamental/Practicas/js/java11.js
+++ PrimerDepartamental/Practicas/js/java11.js
@@ -1,11 +1,18 @@
-function actualizarEtiqueta(idTXT, idP) {
+// Función para actualizar los detalles de compra
+function actualizarEtiqueta() {
+    // Obtener el valor del campo "Nombre Completo" y actualizar el elemento correspondiente en los detalles de compra
     var texto = document.getElementById("nombre").value;
     document.getElementById("nombre-compra").innerText = texto;
+
+    // Obtener el valor del campo "Correo Electrónico" y actualizar el elemento correspondiente en los detalles de compra
     texto = document.getElementById("correo").value;
     document.getElementById("correo-compra").innerText = texto;
+
+    // Obtener el valor del campo "Número de Teléfono" y actualizar el elemento correspondiente en los detalles de compra
     texto = document.getElementById("telefono").value;
     document.getElementById("telefonoo-compra").innerText = texto;
+
+    // Obtener el valor del campo "Tipo de Entrada" y actualizar el elemento correspondiente en los detalles de compra
     texto = document.getElementById("tipo-entrada").value;
     document.getElementById("entrada-compra").innerText = texto;
 }
-
